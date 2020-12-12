168ac8b355eca6237e07abfd82d2ef0cd9c064a3
unknown <nigarofe@gmail.com>
2020-12-12 13:21:09 -0300

    Fim vídeo 27

--- app/src/main/java/com/ifmg/carteiramensal/ItemListaEventos.java
+++ app/src/main/java/com/ifmg/carteiramensal/ItemListaEventos.java
@@ -1,6 +1,7 @@
 package com.ifmg.carteiramensal;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -14,12 +15,12 @@ import java.util.ArrayList;
 public class ItemListaEventos extends ArrayAdapter {
 
     Context contexto;
-    ArrayList<Evento> dadosDosEventos;
+    ArrayList<Evento> eventos;
 
-    public ItemListaEventos(@NonNull Context contexto, ArrayList<Evento> dadosDosEventos) {
+    public ItemListaEventos(@NonNull Context contexto, ArrayList<Evento> eventos) {
         super(contexto, resource);
         this.contexto = contexto;
-        this.dadosDosEventos = dadosDosEventos;
+        this.eventos = eventos;
     }
 
     private static class ViewHolder{
@@ -32,7 +33,28 @@ public class ItemListaEventos extends ArrayAdapter {
 
     @NonNull
     @Override
+    // position = índice do item na lista (parent)
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        //return super.getView(position, convertView, parent);
+        Evento eventoAtual = eventos.get(position);
+        ViewHolder novaView;
+        final View resultado;
+
+        // Se a lista está sendo montada pela primeira vez
+        if (convertView == null){
+            novaView = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView=  inflater.inflate(R.layout.item_lista_eventos, parent, false);
+
+            novaView.nomeTxt = (TextView) convertView.findViewById(R.id.nome);
+            novaView.valorTxt = (TextView) convertView.findViewById(R.id.valorItem);
+            novaView.dataTxt = (TextView) convertView.findViewById(R.id.dataItem);
+            novaView.repeteTxt = (TextView) convertView.findViewById(R.id.repeteItem);
+            novaView.fotoTxt = (TextView) convertView.findViewById(R.id.fotoItem);
+
+            resultado = convertView;
+            convertView.setTag(novaView);
+        }
+        return resultado;
     }
 }
